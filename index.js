b7607a422f81dae3f4949985b41b1335210c1205
Tiffany Horton <tiffanyshorton1@gmail.com>
2019-11-27 11:47:46 -0500

    time

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var time = today.getHours() + ":" + today.getMinutes();
 document.getElementById('time').innerHTML= Date();
