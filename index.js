8865e560fd4d228572d2d93fff1273d4ba5f74b1
Tiffany Horton <tiffanyshorton1@gmail.com>
2019-11-27 18:40:20 -0500

    battery

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function startTime() {
     document.getElementById('time').innerHTML = h + ":" + m;
 }
   startTime();
+function chargebattery() {
+    var a;
+    a = document.getElementById("charging");
+    a.innerHTML = "&#xf244;";
+    setTimeout(function () {
+      a.innerHTML = "&#xf243;";
+    }, 1000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf242;";
+    }, 2000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf241;";
+    }, 3000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf240;";
+    }, 4000);
+  }
+  chargebattery();
+  setInterval(chargebattery, 5000);
