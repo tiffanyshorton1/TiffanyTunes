6a1219f4dee8bec41fbc46d138c13753d4ad9816
Tiffany Horton <tiffanyshorton1@gmail.com>
2019-11-28 13:28:40 -0500

    fix

---
+++ index.js
@@ -0,0 +1,34 @@
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+}
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    document.getElementById('time').innerHTML = h + ":" + m;
+}
+  startTime();
+function chargebattery() {
+    var a;
+    a = document.getElementById("charging");
+    a.innerHTML = "&#xf244;";
+    setTimeout(function () {
+      a.innerHTML = "&#xf243;";
+    }, 1000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf242;";
+    }, 2000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf241;";
+    }, 3000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf240;";
+    }, 4000);
+  }
+  chargebattery();
+  setInterval(chargebattery, 5000);
